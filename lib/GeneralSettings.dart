ff6e006b20e7a57240811bc3d743e265b4963656
Gustavo R. Belumat <gustavoromanha@live.com>
2020-01-17 18:56:30 -0300

    Initial Phase Commit

---
+++ lib/GeneralSettings.dart
@@ -0,0 +1,7 @@
+import 'package:burton_x/Model/User/UserDetailsModel.dart';
+
+class GeneralSettings {
+  static String apiTarget = "APIURL";
+  static String theme = "THEMEX";
+  static UserDetailsModel user;
+}
---
+++ lib/Repo/Event/EventRepository.dart
@@ -0,0 +1,26 @@
+
+import 'package:dio/dio.dart';
+import 'package:burton_x/GeneralSettings.dart';
+import 'package:burton_x/Model/Event/EventListItemModel.dart';
+import 'package:burton_x/Model/Event/DetailedEventModel.dart';
+
+class EventRepository {
+
+  Future<List<EventListItemModel>> listEvents() async { 
+    var targetUrl = "${GeneralSettings.apiTarget}/events";
+    Response response = await Dio().get(targetUrl);
+    return (response.data as List)
+    .map((course)=> EventListItemModel.fromJson(course))
+    .toList(); 
+    //This method gets the simplified available event list.
+  }
+
+  Future<DetailedEventModel> getDetailedEvent(double eventID) async { 
+    var targetUrl = "${GeneralSettings.apiTarget}/events/$eventID";
+    Response response = await Dio().get(targetUrl);
+    return DetailedEventModel.fromJson(response.data);
+    //This method gets the detailed event through eventID.
+  }
+
+
+}
